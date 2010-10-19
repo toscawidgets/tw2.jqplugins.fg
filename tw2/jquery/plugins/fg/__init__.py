3d87a33bc4cec7ed48d8d5bc533ba372c5a2c3e6
Ralph Bean <ralph.bean@gmail.com>
2010-10-19 16:12:20 -0400

    updated link

--- tw2/jquery/plugins/fg/__init__.py
+++ tw2/jquery/plugins/fg/__init__.py
@@ -1,6 +1,6 @@
 """ TW2 wrappers for the widgets from 'filament group' widgets
 
-Get the source from http://github.com/ralphbean/tw2.jquery_fg
+Get the source from http://github.com/ralphbean/tw2.jquery.plugins.fg
 """
 
 from widgets import *
